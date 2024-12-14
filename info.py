97ea72b43a5dff018bb4b1ceecd6ee2b8a087e75
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2024-12-14 18:54:24 +0400

    Update info.py

--- info.py
+++ info.py
@@ -105,7 +105,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "flaky-dinnie-mongodb7575-3c8f5c36.koyeb.app/")
+URL = environ.get("FQDN", "multiple-lacee-mongodb7575-68180570.koyeb.app/")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
