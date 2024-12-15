e61bc2a3a01f4c125f01d7162fd3be3b5f890552
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2024-12-15 13:45:26 +0400

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7874651138:AAGtO65vMlAaga7ho0BymsaZQ3tT0dL
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1275850738').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/ExTra_OrdiNaRy_BoY") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002403428879'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002177262481'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/movie_ott_film_group')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002164616984').split()]
 #---------------------------------------------------------------
@@ -31,10 +31,10 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #----------- There will be channel id add in all these ---------
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002403428879'))  
+LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002177262481'))  
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL','0'))
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0'))
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002403428879'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002177262481'))
 auth_channel = environ.get('AUTH_CHANNEL', '-1001817427847')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
 request_channel = environ.get('REQUEST_CHANNEL', '0')
