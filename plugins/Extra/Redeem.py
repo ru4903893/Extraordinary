2e60e1c8c58b6e64e04172d7dbb211b5503e1313
ru4903893 <154866396+ru4903893@users.noreply.github.com>
2024-12-16 06:25:43 +0400

    Update Redeem.py

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -44,7 +44,7 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
 
         keyboard = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="http://t.me/@Vai_store_Bot")],
+                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="https://t.me/ottfilesender_bot")],
                 [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/ExTra_OrdiNaRy_BoY")]
             ]
         )
